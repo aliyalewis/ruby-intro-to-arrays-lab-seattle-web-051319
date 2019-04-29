2bf414e916ae542c42c4ee9b3917e72fea41c2cd
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-29 19:21:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,8 @@ def array_with_two_elements
   array_with_two_elements = [1,2]
   @my_two_array = array_with_two_elements
 end
+
+def first_element(array)
+  my_first_element = first_element(@taylor_swift)
+  my_first_element[0]
+end
